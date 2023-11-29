6ba023114f767f411df93ca01ca8918e068d41b5
punixcorn <ohyunwoods663@gmail.com>
2023-11-29 17:03:06 +0000

    made some changes

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -12,23 +12,28 @@ done
 xsetroot -cursor_name left_ptr 2>/dev/null
 
 # Polkit agent
-/usr/lib/xfce-polkit/xfce-polkit &
+[ -f /usr/lib/xfce-polkit/xfce-polkit ] && /usr/lib/xfce-polkit/xfce-polkit &
 
 # Enable power management
-xfce4-power-manager &
+# xfce4-power-manager &
 
 # Restore wallpaper
-feh ~/.config/i3/wallpaper/wallpaper.png --bg-fill &
+[ -f /bin/feh ] && feh ~/.config/i3/wallpaper/wallpaper.png --bg-fill &
 
 # Lauch notification daemon
-~/.config/i3/bin/i3dunst.sh
+[ -f /bin/dunst ] && ~/.config/i3/bin/i3dunst.sh
 
 # Lauch polybar
-polybar -c ~/.config/i3/polybar/config.ini &
+[ -f /bin/polybar ] && polybar -c ~/.config/i3/polybar/config.ini &
 disown
 
 # Lauch compositor
 #picom --conf ~/.config/i3/picom.conf --experimental-backends & disown
 
 # Start mpd
-exec mpd &
+[ -f /bin/mpd ] && exec mpd &
+
+# i3lock
+
+[ -f /bin/xss-lock ] && [ -f /bin/i3lock ] && xss-lock --transfer-sleep-lock -- i3lock -i ~/.wallpaper.png --nofork &
+disown
--- installDependencies.sh
+++ installDependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S i3-wm rofi networkmanager-dmenu-git light mpd alacritty polybar dmenu dunst feh --noconfirm
+sudo pacman -S i3-wm rofi networkmanager-dmenu-git light mpd alacritty polybar dmenu dunst feh i3lock xss-lock xsettingsd xsettings-client --noconfirm
+[ -f /bin/yay ] && yay -S xfce-polkit
 
 echo "setting up networkmanager-dmenu-git"
 [ ! -d ~/.config/networkmanager-dmenu/ ] && mkdir ~/.config/networkmanager-dmenu/
